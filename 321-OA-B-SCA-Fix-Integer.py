23fe94b3f6529788e9e1bd24808f33604fbc60b3
Wenbo <wenbo.ma@foxmail.com>
2023-03-21 18:44:35 -0400

    larger T

--- 321-OA-B-SCA-Fix-Integer.py
+++ 321-OA-B-SCA-Fix-Integer.py
@@ -187,7 +187,7 @@ def is_searcher_occ(C, T, grid_size):
 """ Start of the optimization problem  formulation """
 
 grid_size = 9
-ending_time_grid = [10, 12, 14, 15] #, 20] # , 15, 16, 17, 18, 20]
+ending_time_grid = [16, 17, 18, 20] #, 20] # , 15, 16, 17, 18, 20]
 # ending_time_grid = [10, 12, 14, 15] #, 16, 17, 18, 20] # , 15] # , 12, 14]
 # ending_time_grid = [10, 12, 14, 15]
 # , 16, 17, 18, 20]
